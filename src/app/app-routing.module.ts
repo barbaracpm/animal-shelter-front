59e485df8a1befff93bd83828fe54ec9b1dbf283
barbaracpm <barbara-ana.moran-ruiz-ramos@capgemini.com>
2021-12-19 15:21:20 +0100

    add routing, header and footer styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AnimalsComponent } from "./pages/animals/animals.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { NewComponent } from "./pages/new/new.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch:'full'
+
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'animals',
+    component: AnimalsComponent
+  },
+
+  {
+    path: 'animals/new',
+    component: NewComponent
+  },
+  {
+    path:'animals/new/:id',
+    component: NewComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+//VERIFICA SI EXISTE LA RUTA. SI NO EXISTE TE REDIRIGE AL PPAL
+  {
+    path: '**',
+    redirectTo: '/home'
+
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule {
+
+
+ }
