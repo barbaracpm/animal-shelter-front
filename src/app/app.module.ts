9c48ebb33abffe6903602d33dd0f71e767d0b9b3
barbaracpm <barbara-ana.moran-ruiz-ramos@capgemini.com>
2021-12-19 14:40:11 +0100

    add components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewComponent } from './pages/new/new.component';
+import { AnimalsComponent } from './pages/animals/animals.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NewComponent,
+    AnimalsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
